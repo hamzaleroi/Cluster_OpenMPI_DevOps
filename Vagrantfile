6dfdd64c7cb41e9fea4c3dadd26d08a3b10a35dd
hamza <gh_benyamina@esi.dz>
2020-01-13 22:06:32 +0100

    cleaned the folders

--- Vagrantfile
+++ Vagrantfile
@@ -12,7 +12,7 @@ Vagrant.configure("2") do |config|
   config.vm.define "master" do |node|
     node.vm.network "private_network", ip:"10.0.0.5"
     node.vm.provision "ansible" do |ansible|
-      ansible.playbook = "create_cluster.yml"
+      ansible.playbook = "site.yml"
       ansible.limit = "all"
       ansible.vault_password_file = "password"
     end
--- mpi_program/performance_mesure.py
+++ mpi_program/performance_mesure.py
@@ -6,9 +6,15 @@ epochs = (int)((max_size - size) /1500)
 structures = [1,2]
 with open("results.csv","w+") as f:
   for j in structures:
+    
     for i in range(epochs):
-      proc = subprocess.Popen(['mpiexec', '--mca', 'btl_tcp_if_include', 'eth1', '--np', str(j),\
-        '-hostfile', 'machines', 'parallel', str(size + i*100) ], stdout = subprocess.PIPE)
+      proc = subprocess.Popen(['mpiexec', \
+        '--mca', 'btl_tcp_if_include', 'eth1', \
+          '--np', str(j),\
+        '-hostfile', 'machines', \
+          'parallel', str(size + i*100) ]\
+            , stdout = subprocess.PIPE)
+
       out = proc.communicate()[0]
       array = out.decode('ascii').strip().split(',')
       if i == 0 and j == structures[0]: 
